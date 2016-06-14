6a17db5ed8adf7fbab211d07fe1145bd485f8ec7
rms <rooqmeansquare@yandex.ru>
2016-06-14 14:01:16 +0500

    еще раз

---
+++ _Atmel/DVA141_(4_20)/App/source/Sinusoid.h
@@ -0,0 +1,25 @@
+/*
+ * Sinusoid.h
+ *
+ * Created: 14.06.2016 13:52:04
+ *  Author: savchenkors
+ */ 
+
+
+#ifndef SINUSOID_H_
+#define SINUSOID_H_
+
+
+class Sinusoid
+{
+	
+	public:
+	
+	Sinusoid(void);
+	virtual ~Sinusoid(void);
+	
+	static void Sinus(void *pvParameters);
+};
+
+
+#endif /* SINUSOID_H_ */
