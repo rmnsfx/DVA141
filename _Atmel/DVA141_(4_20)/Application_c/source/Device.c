70353e9223a5b754968bf51977973baa5d386590
rms <rooqmeansquare@yandex.ru>
2016-06-27 16:55:42 +0500

    task

--- _Atmel/DVA141_(4_20)/Application_c/source/Device.c
+++ _Atmel/DVA141_(4_20)/Application_c/source/Device.c
@@ -8,9 +8,11 @@
 #include "TaskManager.h"
 #include "Axelerometer.h"
 #include "Device.h"
+#include "AD5421.h"
 
-
+void Testtask(void *pvParameters);
 void RunDeviceTask(void *pvParameters);
+
 #define RUN_DEVICE_STACK_SIZE (2*configMINIMAL_STACK_SIZE)
 
 	/*функции для работы вывода printf*/
@@ -28,7 +30,8 @@ void RunDeviceTask(void *pvParameters);
 
 void RunDevice(void )
 {	
-	xTaskCreate(RunDeviceTask, "RunDevice", RUN_DEVICE_STACK_SIZE, (void *)100, tskIDLE_PRIORITY + 1, NULL);
+	//xTaskCreate(RunDeviceTask, "RunDevice", RUN_DEVICE_STACK_SIZE, (void *)100, tskIDLE_PRIORITY + 1, NULL);
+	xTaskCreate(Testtask, "Test_ad5421", RUN_DEVICE_STACK_SIZE, (void *)100, tskIDLE_PRIORITY + 1, NULL);
 	vTaskStartScheduler();
 }
 
@@ -41,4 +44,20 @@ void RunDeviceTask(void *pvParameters)
 		vTaskDelay(100);
 	}
 	
+}
+
+void Testtask(void *pvParameters)
+{
+	AD5421_Init();
+	for (;;)
+	{		
+		port_pin_set_output_level(PIN_PA24, 1);
+
+		AD5421_SetRegisterValue(AD5421_REG_DAC_DATA, 65535);
+		
+		port_pin_set_output_level(PIN_PA24, 0);		
+		for(int i=0; i<2500; i++) {};
+		port_pin_set_output_level(PIN_PA24, 1);		
+	
+	}
 }
